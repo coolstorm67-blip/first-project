3c466f98b35d290f3e505fbfe45cba790dec880d
coolstorm67-blip <coolstorm67@gmail.com>
2026-08-26 13:09:50 +0100

    first project

---
+++ first project/Welcome1/welcome.cs
@@ -0,0 +1,35 @@
+                         
+using System;   #
+
+namespace MyFirstProgram
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+        double num = 0;
+        double num = 0;
+        double result = 0;
+
+        Console.WriteLine("-----------------"); 
+        Console.WriteLine("Calculator Program");
+        Console.WriteLine("-----------------"); 
+
+        Console.WriteLine( "Enter number 1:");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter number 2;");
+        num2 =convert. ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an option: ");
+        Console .WriteLine ("+: Add );
+        Console.WriteLine("- :Subtract")
+
+        Console . Readkey();
+        }
+    }
+}
+
+
+         
